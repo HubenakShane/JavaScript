0206a20f784fd58a5d7859ffc89e4207c5dd8b1f
lim5 <lim5@lim5s-iMac.local>
2021-08-27 14:24:08 -0600

    ask BSA

--- Js.js
+++ Js.js
@@ -1,60 +1,54 @@
 var button = document.getElementById("m_generate")
 button.addEventListener("click", function () {
-  console.log("hello")
+  console.log(
+    
+    Var length = (len)?(len):len(10)
+    Var length = (len)?(len):len(10);
+    Var string = “abcdefghijklmnopqrstuvwxyz” 
+    Var numeric ‘0123456789’
+    Var punctuation = '@#$&*/=+-%€§£|¥~_…^\!?';
+    Var password = “” ;
+    Var Character = “” ;
+    Var crunch = true;
+    While( password.length<length )
+
+    {Entity1 = Math.Cell(string.length * Math.random()*Math.random());
+    
+      Entity2 = Math.Ceil(numerical.length * Math.random() Math.random());
+    
+      Entity3 = Math.Ceil(punctuation.length * Math.random()*Math.random());
+    
+      hold = string.charAt(entity1 ); 
+
+      hold = (password.length%2==0)?(hold.toUpperCase()):(hold);
+    
+      character += hold;
+    
+      character += numeric.charAt( entity2 );
+    
+      character += punctuation.charAt( entity3 )
+    
+      password = character ;
+    
+    }
+
+
+    Password=Password.split(‘’j.sort(function() (return 0.5-Math.random()}).join(‘’j;
+
+      return password.substr(0,len);
+
+
+
+      *,
+
+      *::before,
+
+      *::after {
+
+        box-sizing: border-box;
+
+      }
+  // to upper
+    ;)
 });
-  //do stuff
-//
-
-// Var length = (len)?(len):len(10);
-
-// Var string = “abcdefghijklmnopqrstuvwxyz” //to upper
-
-// Var numeric ‘0123456789’
-
-// Var punctuation = '@#$&*/=+-%€§£|¥~_…^\!?';
-
-// Var password = “” ;
-
-// Var Character = “” ;
-
-// Var crunch = true;
-
-// While( password.length<length )
-
-// {
-
-//                  Entity1 = Math.Cell(string.length * Math.random()*Math.random());
-
-//                   Entity2 = Math.Ceil(numerical.length * Math.random() Math.random());
-
-//                   Entity3 = Math.Ceil(punctuation.length * Math.random()*Math.random());
-
-//                   hold = string.charAt(entity1 ); 
-
-//                   hold = (password.length%2==0)?(hold.toUpperCase()):(hold);
-
-//                   character += hold;
-
-//                   character += numeric.charAt( entity2 );
-
-//                   character += punctuation.charAt( entity3 )
-
-//                                            password = character ;
-
-// }
-
-// Password=Password.split(‘’j.sort(function() (return 0.5-Math.random()}).join(‘’j;
-
-//           return password.substr(0,len);
-
-
-
-// *,
-
-// *::before,
-
-// *::after {
-
-//   box-sizing: border-box;
-
-// }
+  
