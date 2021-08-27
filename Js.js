184aa6f015698da9e2ffa19f1da9eeede098dfff
lim5 <lim5@lim5s-iMac.local>
2021-08-27 15:56:00 -0600

    ask BCS

--- Js.js
+++ Js.js
@@ -1,54 +1,52 @@
 var button = document.getElementById("m_generate")
-button.addEventListener("click", function () {
-  console.log(string,length)
-    
-    var length = (len)?(len):len(10)
-    var length = (len)?(len):len(10);
-    var string = “abcdefghijklmnopqrstuvwxyz” 
-    var numeric ‘0123456789’
-    var punctuation = '@#$&*/=+-%€§£|¥~_…^\!?';
-    var password = “” ;
-    var character = “” ;
-    var crunch = true;
-    while( password.length<length )
+button.addEventListener("click", function () { 
 
-    {entity1 = Math.Cell(string.length * Math.random()*Math.random());
-    
-      entity2 = Math.Ceil(numerical.length * Math.random() Math.random());
-    
-      entity3 = Math.Ceil(punctuation.length * Math.random()*Math.random());
-    
-      var hold = string.charAt(entity1 ); 
-
-      hold = (password.length%2==0)?(hold.toUpperCase()):(hold);
-    
-      character += hold;
-    
-      character += numeric.charAt( entity2 );
-    
-      character += punctuation.charAt( entity3 )
-    
-      password = character ;
-    
-    }
-
-
-    Password=Password.split(‘’j.sort(function() (return 0.5-Math.random()}).join(‘’j;
-
-      return password.substr(0,len);
-
-
-
-      *,
-
-      *::before,
-
-      *::after {
-
-        box-sizing: border-box;
-
-      }
-  // to upper
-    ;
-});
   
+  var string = 'abcdefghijklmnopqrstuvwxyz';
+  var numeric = '0123456789'
+  var punctuation = '@#$&*/=+-%€§£|¥~_…^\!?';
+  var password = '';
+  var character = '';
+  var crunch = true;
+console.log(string,numeric)
+//   while (password.length < length) {
+//     entity1 = Math.Cell(string.length * Math.random() * Math.random());
+
+//     entity2 = Math.Ceil(numerical.length * Math.random() Math.random());
+
+//     entity3 = Math.Ceil(punctuation.length * Math.random() * Math.random());
+
+//     var hold = string.charAt(entity1);
+
+//     hold = (password.length % 2 == 0) ? (hold.toUpperCase()) : (hold);
+
+//     character += hold;
+
+//     character += numeric.charAt(entity2);
+
+//     character += punctuation.charAt(entity3)
+
+//     password = character;
+
+//   }
+
+
+//   Password = Password.split(‘’j.sort(function () (return 0.5 - Math.random()
+})
+
+// return password.substr(0, len);
+
+
+
+//       *,
+
+//       *:: before,
+
+//       *:: after {
+
+//   box - sizing: border - box;
+
+// }
+// // to upper
+// ;
+// });
